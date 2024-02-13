c431f61aaaea9897460a395cc5e4d8f8ab7e9aea
S-Ihara <shun27182@gmail.com>
2024-02-13 11:00:53 +0900

    c

--- assets/js/pongwar.js
+++ assets/js/pongwar.js
@@ -19,6 +19,9 @@ const ballSpeedX = 5;
 const ballSpeedY = 5;
 let balls = [];
 
+// 時計再表示の間隔（秒）
+const clock_interval = 10;
+
 // それぞれの位置にのブロック情報を格納する配列
 let blocks = [];
 for (let y = 0; y < blockHeightCount; y++) {
@@ -47,6 +50,78 @@ const Color2idx = {
     [Colors.blue]: 2,
 };
 
+// 数字のピクセル情報を格納 (4x6)
+const number_pixels = [
+    [[0,1,1,0],
+     [1,0,0,1],
+     [1,0,1,1],
+     [1,1,0,1],
+     [1,0,0,1],
+     [0,1,1,0],], // 0
+    [[0,1,0,0],
+     [1,1,0,0],
+     [0,1,0,0],
+     [0,1,0,0],
+     [0,1,0,0],
+     [1,1,1,0],], // 1
+    [[0,1,1,0],
+     [1,0,0,1],
+     [0,0,1,0],
+     [0,1,0,0],
+     [1,0,0,0],
+     [1,1,1,1],], // 2
+    [[0,1,1,0],
+     [1,0,0,1],
+     [0,0,1,0],
+     [0,0,0,1],
+     [1,0,0,1],
+     [0,1,1,0],], // 3
+    [[0,0,1,0],
+     [0,1,1,0],
+     [1,0,1,0],
+     [1,1,1,1],
+     [0,0,1,0],
+     [0,0,1,0],], // 4
+    [[1,1,1,1],
+     [1,0,0,0],
+     [1,1,1,0],
+     [0,0,0,1],
+     [1,0,0,1],
+     [0,1,1,0],], // 5
+    [[0,1,1,0],
+     [1,0,0,1],
+     [1,0,0,0],
+     [1,1,1,1],
+     [1,0,0,1],
+     [0,1,1,0],], // 6
+    [[1,1,1,1],
+     [0,0,0,1],
+     [0,0,1,0],
+     [0,1,0,0],
+     [1,0,0,0],
+     [1,0,0,0],], // 7
+    [[0,1,1,0],
+     [1,0,0,1],
+     [0,1,1,0],
+     [1,0,0,1],
+     [1,0,0,1],
+     [0,1,1,0],], // 8
+    [[0,1,1,0],
+     [1,0,0,1],
+     [0,1,1,1],
+     [0,0,0,1],
+     [1,0,0,1],
+     [0,1,1,0],], // 9
+    [[0,0,0,0],
+     [0,0,0,0],
+     [0,1,0,0],
+     [0,0,0,0],
+     [0,1,0,0],
+     [0,0,0,0],] // :
+]
+
+let cnt = 0;
+
 function setup() {
     // メインキャンバスを作成する
     canvas = createCanvas(canvasWidth, canvasHeight);
@@ -57,8 +132,12 @@ function setup() {
     // 適当な位置にボールを配置
     ballX = 95;
     ballY = 100;
-    balls.push(new Ball(ballX, ballY, ballSpeedX, ballSpeedY, Colors.blue));
+    let current_ballSpeedX = ballSpeedX + Math.random() * 4 - 2;
+    let current_ballSpeedY = ballSpeedY + Math.random() * 4 - 2;
+    balls.push(new Ball(ballX, ballY, current_ballSpeedX, current_ballSpeedY, Colors.blue));
     balls.push(new Ball(500, 24, -ballSpeedX, -ballSpeedY, Colors.red));
+
+    drawClock(); // 時計を描画する
 
     // サブキャンバスをid="defaultCanvas1"に追加、そのうちね
 
@@ -79,6 +158,12 @@ function draw() {
 
     display_info();
 
+    cnt += 1;
+    // clock_interval秒ごとに時計を描画する    
+    if (cnt % (fps*clock_interval) == 0){
+        drawClock();
+        cnt = 0;
+    }
 }
 
 class Ball {
@@ -130,7 +215,9 @@ class Ball {
         
         // ブロックに当たったら跳ね返る
         // 当たったブロックは自分と違う色に変更する
-        if (blocks[blockY][blockX] == this.color_idx) {
+
+        // ブロックがボールの色と同じか0の場合は跳ね返る
+        if (blocks[blockY][blockX] == this.color_idx || blocks[blockY][blockX] == 0) {
             let isHitBlock = false;
             if (current_blockX != blockX) {
                 this.speedX *= -1;
@@ -154,7 +241,7 @@ class Ball {
 }
 
 // blocksから描画する関数
-// 1: red 2: blue
+// 0: black 1: red 2: blue
 function drawBlocks() {
     for (let x = 0; x < blockWidthCount; x++) {
         for (let y = 0; y < blockHeightCount; y++) {
@@ -167,8 +254,59 @@ function drawBlocks() {
                 fill(Colors.blue);
                 rect(x * blocksize, y * blocksize, blocksize, blocksize);
             }
+            if (blocks[y][x] == 0) {
+                fill(Colors.black);
+                rect(x * blocksize, y * blocksize, blocksize, blocksize);
+            }
         }
     }
+}
+
+// blocksに時計情報を描画する関数
+function drawClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    //let second = now.getSeconds();
+    // hourとminuteが一桁の場合は0を追加する
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    else{hour = hour.toString();}
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    else{minute = minute.toString();}
+    let time = [hour, minute];
+    let start_x = -1;
+    let start_y = 5;
+    let size = 1;
+    for (let i = 0; i < time.length; i++) {
+        let num = time[i];
+        for (let j = 0; j < num.length; j++) {
+            let digit = parseInt(num[j]);
+            start_x += 5;
+            for (let dy=0; dy<6; dy++){
+                for (let dx=0; dx<4; dx++){
+                    if (number_pixels[digit][dy][dx] == 1){
+                        //blocks[start_y + dy][draw_x + dx] = 0;
+                        blocks[start_y + dy][start_x + dx] = 0;
+                    }
+            }
+        }
+    }
+    if (i == 0){
+        start_x += 5;
+        // 時計の中間の:を描画
+        for (let dy=0; dy<6; dy++){
+            for (let dx=0; dx<4; dx++){
+                if (number_pixels[10][dy][dx] == 1){
+                    blocks[start_y + dy][start_x + dx] = 0;
+                }
+            }
+        }
+    }
+}
 }
 
 // マウスクリックしたらボールを追加する
@@ -176,7 +314,9 @@ function mouseClicked() {
     let point_color = blocks[Math.floor(mouseY / blocksize)][Math.floor(mouseX / blocksize)];
     // 場所の色と違う色のボールを追加する
     let ball_color = point_color == 2 ? Colors.red : Colors.blue;
-    balls.push(new Ball(mouseX, mouseY, ballSpeedX, ballSpeedY, ball_color));
+    let current_ballSpeedX = ballSpeedX + Math.random() * 4 - 2;
+    let current_ballSpeedY = ballSpeedY + Math.random() * 4 - 2;
+    balls.push(new Ball(mouseX, mouseY, current_ballSpeedX, current_ballSpeedY, ball_color));
 }
 
 // utils
@@ -185,6 +325,3 @@ function display_info(){
     // todo そのうち
 
 }
-
-
-
